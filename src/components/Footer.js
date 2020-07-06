29e89a3e1479cfc96377b481ab64703bab794657
lucs123 <lucasf854@gmail.com>
2020-07-06 20:40:44 -0300

    kept the dawn footer at the bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,29 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-
-function Footer(props) {
-	const styles = {
-  		container: {
-  			padding: '20px',
-  			backgroundColor: 'white',
-  			color: 'white',
-  			borderTop: '3px solid'
-  			},
-
-  		 text:{
-		    margin: 'auto',
-		    textAlign: 'center',
-		  },
-
-		  textAlign: {
-		    position: 'absolute', 
-		    left: '95%', 
-		    bottom: '50%',
-		    transform: 'translate(-50%, -50%)'
-		  }
-	}
-
-	// const lineBR = {
-	// 	// lineHeight: '150px'
-	// 	height
-	// }
-
-	return(
-			<footer style={styles.container}>
-			    <Typography variant="body2" color="textSecondary" align="center">
-			      {'© Copyright 2020 Lucas ferreira'}
-			    </Typography>
-			    <Typography variant="body2" color="textSecondary" align="center">
-			      {'Email:lucasf854@gmail.com'}
-			    </Typography>
-			    <Typography variant="body2" color="textSecondary" align="center">
-			      {'Whatsapp:(11)95975-6032'}
-			    </Typography>
-    		</footer>
-		)
+const footer = {
+	padding: "20px",
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
 }
-            // <img src="https://img.icons8.com/small/24/000000/facebook.png" alt='Facebook' className={classes.logo} aria-label="Facebook"/>
-            // <img src="https://img.icons8.com/small/24/000000/whatsapp.png" alt='Whatsapp' className={classes.logo} aria-label="Whatsapp"/>
+function Footer(props) {
+
+    return (
+    	<div style={footer}>
+        <footer>
+	 		    <Typography variant="body2" color="textSecondary" align="center">
+	 		      {'© Copyright 2020 Lucas ferreira'}
+	 		    </Typography>
+	 		    <Typography variant="body2" color="textSecondary" align="center">
+	 		      {'Email:lucasf854@gmail.com'}
+	 		    </Typography>
+	 		    <Typography variant="body2" color="textSecondary" align="center">
+	 		      {'Whatsapp:(11)95975-6032'}
+	 		    </Typography>    
+        </footer>
+        </div>
+    )
+}
 
 export default Footer;
