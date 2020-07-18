97b13d55178c61971657667792d664d18e29d793
Lucas <lucasf854@gmail.com>
2020-07-18 18:28:06 -0300

    jwt authorization

--- src/auth.js
+++ src/auth.js
@@ -5,8 +5,8 @@ class Auth {
     this.authenticated = false;
   }
 
-  login(email, pass) {
-        (async () => {
+  async login(email, pass) {
+      await  (async () => {
       const rawResponse = await fetch('/login', {
         method: 'POST',
         headers: {
@@ -16,41 +16,36 @@ class Auth {
         body: JSON.stringify({email: email, password: pass})
       });
       const content = await rawResponse.json();
-      // console.log(rawResponse.headers.json())
-      console.log(content.token)
 
       if(content.auth){
         localStorage.setItem(process.env.REACT_APP_TOKEN_KEY, content.token)
       }
     })();
-    // this.authenticated = true;
-    // history.push('/')
+    history.push('/')
   }
 
   logout() {
     this.authenticated = false;
+    localStorage.removeItem(process.env.REACT_APP_TOKEN_KEY)
     history.push('/login')
   }
 
-  async isAuthenticated() {
-      const token = await localStorage.getItem(process.env.REACT_APP_TOKEN_KEY)
+  isAuthenticated() {
+      const token = localStorage.getItem(process.env.REACT_APP_TOKEN_KEY)
       if(token){
-        console.log(token)
-
+      this.authenticated = (async () => {
       const rawResponse = await fetch('/login/auth', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({token: token})
+        body: JSON.stringify({token:token})
       });
       const content = await rawResponse.json();
-      console.log(content)
 
-      // if(content.auth){
-      //   localStorage.setItem(process.env.REACT_APP_TOKEN_KEY, content.token)
-      // }
+      return content.auth
+    })();
 
     }
     return this.authenticated;
